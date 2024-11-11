e09c6fd06f3785fe356cf74d556f897723baf471
Nolan-McKenna <nolan.mckenna1@gmail.com>
2024-11-11 11:25:38 -0500

    Generate Runtime Errors

---
+++ oro/RuntimeError.java
@@ -0,0 +1,10 @@
+package oro;
+
+class RuntimeError extends RuntimeException {
+    final Token token;
+  
+    RuntimeError(Token token, String message) {
+      super(message);
+      this.token = token;
+    }
+  }
