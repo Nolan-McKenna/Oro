55af1081a6efff7c61d510fe11a02fb7d95c2cd3
Nolan-McKenna <nolan.mckenna1@gmail.com>
2024-12-09 15:35:48 -0500

    Support for interpreting retrun types and function declarations

--- oro/Interpreter.java
+++ oro/Interpreter.java
@@ -149,6 +149,13 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+      OroFunction function = new OroFunction(stmt, environment);
+      environment.define(stmt.name.lexeme, function);
+      return null;
+    }
+
+    @Override
     public Void visitIfStmt(Stmt.If stmt) {
       if (isTruthy(evaluate(stmt.condition))) {
         execute(stmt.thenBranch);
@@ -164,6 +171,14 @@ class Interpreter implements Expr.Visitor<Object>,
       System.out.println(stringify(value));
       return null;
     }
+
+    @Override
+    public Void visitReturnStmt(Stmt.Return stmt) {
+      Object value = null;
+      if (stmt.value != null) value = evaluate(stmt.value);
+      throw new Return(value);
+    }
+  
 
     @Override
     public Void visitDefStmt(Stmt.Def stmt) {
