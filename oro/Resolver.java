954613d7d661278d98088700202d6bdba265cb0c
Nolan McKenna <nolan.mckenna1@gmail.com>
2024-12-20 15:20:01 -0500

    Resolve class declarations, enum -> function,class

--- oro/Resolver.java
+++ oro/Resolver.java
@@ -19,8 +19,17 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum FunctionType {
     NONE,
-    FUNCTION
+    FUNCTION,
+    INITIALIZER,
+    METHOD
   }
+
+  private enum ClassType {
+    NONE,
+    CLASS
+  }
+
+  private ClassType currentClass = ClassType.NONE;
 
  @Override
  public Void visitBlockStmt(Stmt.Block stmt) {
@@ -28,6 +37,32 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
    resolve(stmt.statements);
    endScope();
    return null; 
+ }
+
+ @Override
+ public Void visitClassStmt(Stmt.Class stmt) {
+  ClassType enclosingClass = currentClass;
+  currentClass = ClassType.CLASS; 
+  
+  declare(stmt.name);
+   define(stmt.name);
+
+   beginScope();
+   scopes.peek().put("self", true);
+
+   for (Stmt.Function method : stmt.methods) {
+    FunctionType declaration = FunctionType.METHOD;
+    if (method.name.lexeme.equals(stmt.name.lexeme)) {
+      declaration = FunctionType.INITIALIZER;
+    }
+
+    resolveFunction(method, declaration); 
+  }
+
+   endScope();
+
+   currentClass = enclosingClass;
+   return null;
  }
 
  @Override
@@ -66,6 +101,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
    if (stmt.value != null) {
+    if (currentFunction == FunctionType.INITIALIZER) {
+      Oro.error(stmt.keyword, "Can't return a value from an initializer.");
+    }
      resolve(stmt.value);
    }
 
@@ -115,6 +153,13 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
    return null;
  }
 
+ // Property dynamically evaluated, not resolved here
+ @Override
+ public Void visitGetExpr(Expr.Get expr) {
+   resolve(expr.object);
+   return null;
+ }
+
  @Override
  public Void visitGroupingExpr(Expr.Grouping expr) {
    resolve(expr.expression);
@@ -132,6 +177,26 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
    resolve(expr.right);
    return null;
  }
+
+ // Property dynamically evaluated, not resolved here
+ @Override
+ public Void visitSetExpr(Expr.Set expr) {
+   resolve(expr.value);
+   resolve(expr.object);
+   return null;
+ }
+
+ @Override
+ public Void visitSelfExpr(Expr.Self expr) {
+  if (currentClass == ClassType.NONE) {
+    Oro.error(expr.keyword,"Can't use 'self' outside of a class.");
+    return null;
+  }
+
+   resolveLocal(expr, expr.keyword);
+   return null;
+ }
+
 
  @Override
  public Void visitUnaryExpr(Expr.Unary expr) {
