84ad3005a814423b24f62c9e1ff1e3eea0a0764d
Nolan McKenna <nolan.mckenna1@gmail.com>
2024-12-20 15:18:24 -0500

    Add suport for constructors, 'self' keyword

--- oro/OroFunction.java
+++ oro/OroFunction.java
@@ -5,10 +5,18 @@ import java.util.List;
 class OroFunction implements OroCallable {
   private final Stmt.Function declaration;
   private final Environment closure;
+  private final boolean isInitializer;
 
-  OroFunction(Stmt.Function declaration, Environment closure) {
+  OroFunction(Stmt.Function declaration, Environment closure, boolean isInitializer) {
+    this.isInitializer = isInitializer;
     this.closure = closure;
     this.declaration = declaration;
+  }
+
+  OroFunction bind(OroInstance instance) {
+    Environment environment = new Environment(closure);
+    environment.define("self", instance);
+    return new OroFunction(declaration, environment, isInitializer);
   }
 
   @Override
@@ -34,8 +42,11 @@ class OroFunction implements OroCallable {
     try {
         interpreter.executeBlock(declaration.body, environment);
       } catch (Return returnValue) {
+        if (isInitializer) return closure.getAt(0, "self");
         return returnValue.value;
       }
+
+    if (isInitializer) return closure.getAt(0, "self");
     return null;
   }
 
