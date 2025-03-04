fe75efc392e6fc4a9cae7a36fad8fe1d20a8caa0
Nolan McKenna <nolan.mckenna1@gmail.com>
2025-03-04 11:30:29 -0500

    Set serialVersionUID to avoid warnings

--- oro/Return.java
+++ oro/Return.java
@@ -2,6 +2,7 @@
 package oro;
 class Return extends RuntimeException {
     final Object value;
+    private static final long serialVersionUID = 1L;
   
     Return(Object value) {
       super(null, null, false, false);
--- oro/RuntimeError.java
+++ oro/RuntimeError.java
@@ -2,6 +2,7 @@ package oro;
 
 class RuntimeError extends RuntimeException {
     final Token token;
+    private static final long serialVersionUID = 1L;
   
     RuntimeError(Token token, String message) {
       super(message);
